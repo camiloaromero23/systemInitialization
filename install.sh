fe297a081949a0185e0152ceb5eca072fdd94257
Camilo Romero <camiloaromero23@gmail.com>
2020-12-30 16:35:25 -0500

    Fixed issue with utilFunctions file

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 sudo apt update
 sudo apt upgrade -y
 
-. ./utilFunctions.sh
+wget -P ~/Downloads https://raw.githubusercontent.com/camiloaromero23/systemInitialization/main/utilFunctions.sh
+. ~/Downloads/utilFunctions.sh
 
 installGeneralSoftware
 installChrome
 installVSCode
 installNodeJS
 installPython
+
+rm ~/Downloads/utilFunctions.sh
