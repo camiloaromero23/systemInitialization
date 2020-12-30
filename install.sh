f1058e77e65a06ec4f3f2f1b004613031f3db280
Camilo Romero <camiloaromero23@gmail.com>
2020-12-30 16:28:17 -0500

    Included installations for generalSoftware, Chrome, VSCode, NodeJS and Python

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git zsh 
+
+. ./utilFunctions.sh
+
+installGeneralSoftware
+installChrome
+installVSCode
+installNodeJS
+installPython
