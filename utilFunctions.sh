d535b02a7471d7956beb90b2c963e74e96d97c44
Camilo Romero <camiloaromero23@gmail.com>
2020-12-31 17:46:26 -0500

    Changed docker installation scripts to one line instructions

--- utilFunctions.sh
+++ utilFunctions.sh
@@ -58,18 +58,10 @@ installDocker()
 {
     printMessage "Installing Docker..."
     sudo apt update
-    sudo apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt update
     sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
     sudo usermod -aG docker $USER
