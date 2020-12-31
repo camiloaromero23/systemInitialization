532dd448e96c8f7c9920342f3763fe47cffcafee
Camilo Romero <camiloaromero23@gmail.com>
2020-12-30 22:46:18 -0500

    Included variable for installing NodeJS

--- utilFunctions.sh
+++ utilFunctions.sh
@@ -57,6 +57,9 @@ installVSCode()
 installNodeJS()
 {
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+export NVM_DIR="/home/$USER/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install lts/erbium
 nvm install --lts
 }
